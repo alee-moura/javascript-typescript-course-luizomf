0612a61372d8a66a96b03fe9df5499a9b256bb0a
Alexandre <alexandre-cld@outlook.com>
2021-05-20 18:59:11 -0300

    console.log

---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log("'Luiz' 'Otavio'"); // string = texto
+console.log('"Luiz" "Otavio"'); //  string = texto
+console.log(`'Luiz' "Otavio"`);
+
+console.log(35, 15.85, 'Luiz Otavio de novo');
