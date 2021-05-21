42e29dfc1ee583f9acf10a18797e67264b3f1b82
Alexandre <alexandre-cld@outlook.com>
2021-05-21 10:08:32 -0300

    comentarios

---
+++ aula4/comentarios.js
@@ -0,0 +1,11 @@
+// e escrever um comentário longo
+console.log('Olá mundo!'); // aqui tem outro comentário
+console.log('Linha 1');
+
+/* 
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+
+// vai ser ignorado
