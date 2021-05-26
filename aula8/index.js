b129194b452470d6fdae44d9ebd611bb25f571df
Alexandre <alexandre-cld@outlook.com>
2021-05-26 20:30:07 -0300

    correcao exercicio com const e let

--- aula8/index.js
+++ aula8/index.js
@@ -4,6 +4,10 @@ tem 1.8 de altura e seu IMC é de 25.925925925925924
 Luiz Otávio nasceu em 1980
 */
 
+
+/*
+minha versão
+
 const nome = 'Luiz Otávio';
 const sobreNome = 'Miranda';
 const idade = 30;
@@ -14,3 +18,21 @@ const anoNascimento = 1980;
 
 console.log(`${nome} ${sobreNome} tem ${idade} anos, pesa ${peso} kg, tem ${altura} de altura e seu IMC é de ${imc}.
 ${nome} nasceu em ${anoNascimento}`)
+*/
+
+// correção do curso
+const nome = 'Luiz Otávio';
+const sobreNome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const altura = 1.8;
+let imc; // peso / (altura * altura)
+let anoNascimento;
+
+imc = peso / (altura * altura);
+anoNascimento = 2010 - idade;
+
+console.log(nome + ' ' + sobrenome + ' tem' + idade + ' anos, pesa ' + peso + ' kg');
+// template string
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}`);
+console.log(nome, 'nasceu em', anoNascimento, '.');
