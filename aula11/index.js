bb94a299b0da0e51dff330cf92c05c965de1ea2f
Alexandre <alexandre-cld@outlook.com>
2021-05-27 13:46:28 -0300

    operadores aritmeticos, de atribuicao e incremento

---
+++ aula11/index.js
@@ -0,0 +1,52 @@
+/*
+    aritméticos
+    + -> adição / concatenação 
+    - -> subtração
+    / -> divisão
+    * -> multiplicação
+    ** -> potenciação
+    % -> resto da divisão
+*/
+
+/*
+    precedência
+    ()
+    **
+    * / %
+    + -
+*/
+
+/*
+    operadores de incremento (pré e pós)
+let contador = 1;
+contador++; // 2
+++contador; // 3
+contador++; // 4
+++contador; // 5
+console.log(contador);
+
+    operadores de decremento (pré e pós)
+let contador2 = 10;
+console.log(--contador2); // 9
+console.log(--contador2); // 8
+*/
+
+/*
+    operadores de atribuição
+let contador = 2;
+contador *= 2;
+contador *= 2;
+contador *= 2;
+console.log(contador);
+*/
+
+/* 
+    NaN -> not a number
+    parseInt(inteiro)
+    parseFloat(decimais)
+    Number(ambos)
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);
+console.log(typeof num2);
+*/
