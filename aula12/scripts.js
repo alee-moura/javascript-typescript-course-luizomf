842c7c9af9f42589321cd900712d4cab5645008a
Alexandre de MOura Boni <alexandre-cld@outlook.com>
2021-06-20 19:00:52 -0300

    correçao exercicio

--- aula12/scripts.js
+++ aula12/scripts.js
@@ -1,5 +1,18 @@
+
+/* minha versão
 const num1 = Number(prompt('Digite um número'));
 const num2 = Number(prompt('Digite outro número'));
 const resultado = num1 + num2;
 
 alert(`O resultado da soma é: ${resultado}`);
+*/
+
+// correção
+let num1 = prompt('Digite um número');
+let num1 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+alert(`O resultado da sua conta foi ${resultado}`);
