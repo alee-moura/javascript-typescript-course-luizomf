e6ba717f3dadfbc9148eb4a1aa083c2558b048ab
Alexandre de MOura Boni <alexandre-cld@outlook.com>
2021-07-02 20:37:22 -0300

    arrays (basico)

---
+++ aula17/index.js
@@ -0,0 +1,39 @@
+//      0123456789
+const nome = 'Luiz Otávio';
+
+//                 0        1        2       3         4
+const alunos = ['Luiz', 'Mária', 'João']; // Luiza, Eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array); // essa variável é um array?
+
+// alunos.push['Luiza'];
+// alunos.push['Eduardo'];
+
+// console.log(alunos.slice(0, 3));
+
+// delete alunos[1];
+// console.log(alunos);
+
+// const removidoUltimo = alunos.pop();
+// const removidoPrimeiro = alunos.shift();
+
+// alunos.unshift('Luiza'); // adiciona no início
+// alunos.unshift('Fábio'); // adiciona no início
+
+// alunos.push('Otávio'); // adiciona no fim
+// alunos.push('Fábio'); // adiciona no fim
+
+// console.log(removido);
+
+// alunos[alunos.length] = 'Luiza';
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+
+// alunos[0] = 'Eduardo'; // altera
+// alunos[3] = 'Luiza'; // adiciona
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
