a4d04699b9d35633868b689e2ba85f014bfef8e5
Alexandre de MOura Boni <alexandre-cld@outlook.com>
2021-07-02 21:08:58 -0300

    funcoes (basico)

---
+++ aula18/index.js
@@ -0,0 +1,30 @@
+// arrow function
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+
+/*
+const raiz = function (n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+/*
+
+/*
+function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(4, 2));
+*/
+
+/*
+function saudacao(nome) {
+    return `Bom dia, ${nome}`;
+}
+
+const variavel = saudacao('Luiz');
+console.log(variavel);
+*/
