7bca48ca96fbe31e8bda8978e0fcee2aaf426e67
Alexandre de MOura Boni <alexandre-cld@outlook.com>
2021-07-07 19:10:39 -0300

    valores primitivos e valores por referencia

---
+++ aula20/index.js
@@ -0,0 +1,6 @@
+/*
+    Primitivos (imutáveis) - string, number,
+    boolean, undefined, null (bigint, symbol) - valores copiados
+
+    Referência (mutável) - array, object, function - valores passados por referência
+*/
