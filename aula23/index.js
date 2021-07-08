140d4de7bda7fc81e55c92a836bf3b91e335b2f1
Alexandre de MOura Boni <alexandre-cld@outlook.com>
2021-07-08 18:10:13 -0300

    operadores logicos

---
+++ aula23/index.js
@@ -0,0 +1,20 @@
+/*
+    operadores lógicos
+    && -> AND -> E - todas as expressões precisam ser verdadeiras para retornar true
+    || -> OR -> OU - 
+    ! -> NOT -> NÃO
+*/
+
+// const expressaoAnd = true && true && false && true;
+// const expressaoOr = true || false;
+
+/*              exemplo
+const usuario = 'Luiz'; // form usuário digitou
+const senha = '123456'; // form usuário digitou
+
+                    True                    False
+const vaiLogar = usuario === 'Luiz' && senha === '12345';
+console.log(vailogar);
+*/
+
+console.log(!true);
